ca9666693974f3beb2c02e3ec2a4da0c8132ebfe
holaquemas <86824890+holaquemas@users.noreply.github.com>
2022-04-28 09:55:17 -0500

    Add files via upload
    Escribe una función que calcule el área de un triángulo, recibiendo la altura y la base como parámetros y otra función que calcule el área de un círculo recibiendo el radio del mismo.

---
+++ tarea5.py
@@ -0,0 +1,16 @@
+#Area de un triasngulo 
+def area(base,altura):
+   
+    print("El area del triangulo es ", ((base*altura)/2))
+
+area(9,5)
+
+
+#Funcion Area de un circulo
+
+def areac(radio):
+    pi=3.14
+    area=pi*radio**2
+    print("El area del circulo es:", area)
+
+areac(50)
