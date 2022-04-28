d6fd3e01a2603e0b1990917caa75b7d1855a1773
holaquemas <86824890+holaquemas@users.noreply.github.com>
2022-04-28 10:20:43 -0500

    Add files via upload
    Escribe una función que pueda decirte si un número (número entero) es primo o no.

---
+++ tarea5-1.py
@@ -0,0 +1,10 @@
+#Funcion numero es primo
+def es_primo(num):
+    for n in range(2, num):
+        if num % n == 0:
+            print("No es primo", n, "es divisor")
+            return False
+    print("Es primo")
+    return True
+
+es_primo(596)
